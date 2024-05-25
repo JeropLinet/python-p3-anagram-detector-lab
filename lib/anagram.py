0fd1a793f498edaeb80e829af1dd7d874970e15b
Linet <teweilinet@gmail.com>
2024-05-25 09:39:31 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self,words):
+       if not words:
+           return []
+       match_word=[word for word in words if sorted(self.word)==sorted(word)]
+       
+       if not match_word:  
+            return []
+        
+       return match_word
+    
+listen = Anagram("listen")
+
+
